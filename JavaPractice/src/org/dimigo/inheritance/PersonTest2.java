588a6c9b3c18efafa87d9e782cf0404cbdf82a96
dltjdud <dltjdud@DESKTOP-4KHMQUI>
2017-05-12 11:05:45 +0900

    10

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+*  <pre>
+* org.dimigo.inheritance
+*		|_ PersonTest
+*
+* 1. 개요 : 
+* 2. 작성일 : 2017. 5. 10.
+* </pre>
+*
+ * @author   :  dltjdud
+ * @version  : 1.0
+ */
+public class PersonTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	      
+	      Person[] persons = {
+	            new Person("Tom"),
+	            new Korean("홍길동"),
+	            new Japanese("다나카"),
+	            new Chinese("왕밍")
+	      };      
+	      
+	      for (Person person : persons) {
+	         greeting(person);
+	         System.out.println();
+	      }
+	      
+	   }
+	   
+	   private static void greeting(Person person) {
+	      System.out.println(person);
+	      person.sayHello();
+	      person.sayBye();
+	   }
+	   
+}
